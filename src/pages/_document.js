bb9dbe964dcad52449cb95816927695bf9fe878a
Lucas Marques <marques.santoslucas@gmail.com>
2025-04-12 11:49:51 -0300

    Adiciona lista suspensa de CPF, trava nome, adiciona função e diária com botão de adicionar

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
   <link rel="manifest" href="/manifest.json" />
-  <link rel="icon" href="/icon.png" />
+  {/* Removido: <link rel="icon" href="/icon.png" /> */}
   <meta name="theme-color" content="#006633" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="default" />
   <link rel="apple-touch-icon" href="/icon.png" />
 </Head>
+
 
       <body className="antialiased">
         <Main />
