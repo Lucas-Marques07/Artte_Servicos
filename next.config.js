41b48d118cc3d7efd63f31fed3191451d85a824e
Lucas Marques <marques.santoslucas@gmail.com>
2025-04-15 11:40:36 -0300

    Desabilita lightningcss para Windows

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    css: {
+      // Desabilita o lightningcss
+      lightningcss: false
+    }
+  }
 };
 
 module.exports = nextConfig;
