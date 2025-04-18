21b0ef724cefb2f79d963ee0ad580bc10c18a64f
Lucas Marques <marques.santoslucas@gmail.com>
2025-04-18 00:35:38 -0300

    Adiciona lista suspensa de CPF, trava nome, adiciona função e diária com botão de adicionar

--- src/pages/reembolso.js
+++ src/pages/reembolso.js
@@ -1,4 +1,4 @@
-// src/pages/reembolso.js
+// src/pages/marmita/novo.js
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
@@ -7,35 +7,61 @@ export default function NovaMarmita() {
   const [marmitas, setMarmitas] = useState([
     { data: '', horario: '', empresa: '', operacao: '', solicitante: '', quantidade: '', valor: '' , categoria: ''}
   ]);
-  const handleCheckboxChange = () => {
-    setUsarMesmoValor(!usarMesmoValor);
-  };
   const [dadosPlanilha, setDadosPlanilha] = useState([]);
   const [operacoesFiltradas, setOperacoesFiltradas] = useState([]);
-  const [file, setFile] = useState(null);
   const [nomeLogado, setNomeLogado] = useState('');
   const [usarMesmoValor, setUsarMesmoValor] = useState(false);
   const [categoriaUnicos, setcategoriaUnicos] = useState([]);
-  const shareContent = () => {
+  const [file, setFile] = useState(null);
+
+  const compartilharTexto = () => {
+    if (!validarCampos()) return;
+
+    const mensagem = marmitas.map((m, i) => {
+      const dataFormatada = new Date(m.data).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit'
+      });
+
+      return `🧾 Solicitação de Reembolso *${i + 1}º*
+
+🗂️ Categoria: ${m.categoria} | 📅 Data: ${dataFormatada}
+🏢 Empresa: ${m.empresa} | ⚙️ Operação: ${m.operacao}
+💰 Valor: R$ ${m.valor} | 👤 Solicitante: ${m.solicitante}
+
+📝 OBS: ${m.endereço}`;
+
+    }).join('\n-------------------------\n');
+
     if (navigator.share) {
-      const texto = "Estou informando o reembolso de X valor. Comprovante anexado.";
-  
-      if (file) {
-        navigator.share({
-          title: 'Reembolso',
-          text: texto,
-          files: [file],
-        })
-        .then(() => console.log('Compartilhado com sucesso!'))
-        .catch((error) => console.log('Erro ao compartilhar', error));
-      } else {
-        alert('Nenhum arquivo selecionado.');
-      }
+      navigator.share({
+        title: 'Solicitação de Reembolso',
+        text: mensagem,
+      }).catch((err) => console.error("Erro ao compartilhar texto:", err));
     } else {
-      alert("O compartilhamento não é suportado nesse navegador ou contexto.");
+      alert("Compartilhamento de texto não suportado.");
     }
   };
-  
+
+  const compartilharComprovante = () => {
+    if (!file) {
+      alert("Nenhum comprovante selecionado.");
+      return;
+    }
+
+    if (navigator.share) {
+      navigator.share({
+        title: 'Comprovante de Reembolso',
+        files: [file],
+      }).catch((err) => console.error("Erro ao compartilhar imagem:", err));
+    } else {
+      alert("Compartilhamento de arquivos não suportado.");
+    }
+  };
+
+  const handleCheckboxChange = () => {
+    setUsarMesmoValor(!usarMesmoValor);
+  };
 
   useEffect(() => {
     if (dadosPlanilha.length > 0) {
@@ -43,56 +69,49 @@ export default function NovaMarmita() {
       setOperacoesFiltradas(operacoes);
     }
   }, [dadosPlanilha]);
-  
 
-useEffect(() => {
-  if (typeof window !== 'undefined') {
-    const usuario = JSON.parse(localStorage.getItem('usuarioLogado') || '{}');
-    if (usuario?.usuario) {
-      setNomeLogado(usuario.usuario);
-      setMarmitas([{ data: '', horario: '', empresa: '', operacao: '', solicitante: usuario.usuario, quantidade: '', valor: '', endereço: '' }]);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const usuario = JSON.parse(localStorage.getItem('usuarioLogado') || '{}');
+      if (usuario?.usuario) {
+        setNomeLogado(usuario.usuario);
+        setMarmitas([{ data: '', horario: '', empresa: '', operacao: '', solicitante: usuario.usuario, quantidade: '', valor: '', endereço: '' }]);
+      }
     }
-  }
-}, []);
+  }, []);
 
+  useEffect(() => {
+    fetch('https://script.google.com/macros/s/AKfycbzL6NoS3730uOtG1T3SsqpgXVp4r_s9rRDd-c9B85cip9mByzaYEzFwepWxawH1VHni/exec')
+      .then(res => res.json())
+      .then(data => setDadosPlanilha(data));
+  }, []);
 
+  useEffect(() => {
+    if (dadosPlanilha.length > 0) {
+      const categoria = [...new Set(dadosPlanilha.map(item => item['Categoria reembolso']).filter(Boolean))];
+      setcategoriaUnicos(categoria);
+    }
+  }, [dadosPlanilha]);
 
-
-useEffect(() => {
-  fetch('https://script.google.com/macros/s/AKfycbzL6NoS3730uOtG1T3SsqpgXVp4r_s9rRDd-c9B85cip9mByzaYEzFwepWxawH1VHni/exec')
-    .then(res => res.json())
-    .then(data => setDadosPlanilha(data));
-}, []);
-
-// ✅ Esse useEffect vai rodar só quando os dados forem carregados
-useEffect(() => {
-  if (dadosPlanilha.length > 0) {
-    const categoria = [...new Set(dadosPlanilha.map(item => item['Categoria reembolso']).filter(Boolean))];
-    setcategoriaUnicos(categoria);
-  }
-}, [dadosPlanilha]);
-
-const handleChange = (index, campo, valor) => {
-  const novaLista = [...marmitas];
-  novaLista[index][campo] = valor;
-
+  const handleChange = (index, campo, valor) => {
+    const novaLista = [...marmitas];
+    novaLista[index][campo] = valor;
     setMarmitas(novaLista);
-};
+  };
 
-const adicionarLinha = () => {
-  const novaLinha = { data: '', horario: '', empresa: '', operacao: '', solicitante: nomeLogado, quantidade: '', valor: '', endereço: '' };
-  if (usarMesmoValor && marmitas.length > 0) {
-    novaLinha.valor = marmitas[0].valor;
-  }
-  setMarmitas([...marmitas, novaLinha]);
-};
+  const adicionarLinha = () => {
+    const novaLinha = { data: '', horario: '', empresa: '', operacao: '', solicitante: nomeLogado, quantidade: '', valor: '', endereço: '' };
+    if (usarMesmoValor && marmitas.length > 0) {
+      novaLinha.valor = marmitas[0].valor;
+    }
+    setMarmitas([...marmitas, novaLinha]);
+  };
 
-const removerLinha = (index) => {
-  const novaLista = [...marmitas];
-  novaLista.splice(index, 1);
-  setMarmitas(novaLista);
-};
-
+  const removerLinha = (index) => {
+    const novaLista = [...marmitas];
+    novaLista.splice(index, 1);
+    setMarmitas(novaLista);
+  };
 
   const validarCampos = () => {
     for (let m of marmitas) {
@@ -104,234 +123,82 @@ const removerLinha = (index) => {
     return true;
   };
 
-  const handleEnviar = () => {
-    if (!validarCampos()) return;
-  
-    const mensagem = marmitas.map((m, i) => {
-      const dataFormatada = new Date(m.data).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: '2-digit'
-      });
-  
-      return `🍽️ Solicitação de Marmita *${i + 1}º*
-  
-  🏢 *Empresa:* ${m.empresa} | 🛠️ *Operação:* ${m.operacao}
-  📅 *Data:* ${dataFormatada} | ⏰ *Entrega:* ${m.horario}
-  👤 *Quantidade:* ${m.quantidade} | 📌 *Solicitante:* ${m.solicitante}
-  
-  📍 *Endereço:* ${m.endereço}`;
-    }).join('\n-------------------------\n');
-  
-    // Primeiro passo: compartilhar o texto
-    if (navigator.share) {
-      navigator.share({
-        title: 'Solicitação de Reembolso',
-        text: mensagem,
-      })
-      .then(() => {
-        // Segundo passo: compartilhar o arquivo, se existir
-        if (file) {
-          navigator.share({
-            title: 'Comprovante de Reembolso',
-            files: [file],
-          })
-          .catch((err) => {
-            console.warn("Erro ao compartilhar imagem:", err);
-          });
-        } else {
-          alert("Texto enviado. Nenhum comprovante anexado.");
-        }
-      })
-      .catch((error) => {
-        console.error('Erro ao compartilhar o texto:', error);
-      });
-    } else {
-      alert('Compartilhamento não suportado neste navegador.');
-    }
-  };
-  
-  
-  
-
   const empresas = [...new Set(dadosPlanilha.map(d => d.EMPRESA))];
 
   return (
-    <div
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      minHeight: '100vh',
-      background: '#f8f8f8',
-      fontFamily: 'sans-serif',
-      padding: '2rem'
-    }}
-  >
-    
-    <div style={{ background: '#fff', padding: '2rem', borderRadius: '8px', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}>
-      
-      {/* Cabeçalho com logo e botão voltar */}
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '1rem' }}>
-        <img src="/artte.ico" alt="Logo" style={{ height: '110px' }} />
-        <button
-          onClick={() => router.push('/login')}
-          style={{
-            background: '#141e7d',
-            color: '#fff',
-            border: 'none',
-            padding: '8px 10px',
-            borderRadius: '6px',
-            cursor: 'pointer',
-            fontSize: '14px',
-            width: '80px',
-            textAlign: 'center',
-          }}
-        >
-          ⬅ Voltar
-        </button>
-      </div>
-
-      {/* Título */}
-      <center>
-        <h2 style={{ marginBottom: '1rem' }}>Solicitação de Reembolso</h2>
-      </center>
-       {/* Checkbox usar mesmo valor */}
-       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginBottom: '0.5rem', fontSize: '14px' }}>
-  <span style={{ marginRight: '6px' }}>Padrão</span>
-  <input
-    type="checkbox"
-    checked={usarMesmoValor}
-    onChange={handleCheckboxChange}
-    style={{ width: '14px', height: '14px' }}
-  />
-</div>
-             
-{marmitas.map((m, index) => (
-  
-  <div
-    key={index}
-    style={{
-      border: '1px solid #ccc',
-      padding: '10px',
-      borderRadius: '6px',
-      marginBottom: '10px',
-      background: '#f9f9f9',
-      width: '350px'
-    }}
-    
-  >
-    <div style={{ display: 'flex', justifyContent: 'flex-end',  marginBottom: '10px', }}>
-      <span
-        onClick={() => removerLinha(index)}
-        style={{
-          background: 'none',
-          border: 'none',
-          cursor: 'pointer',
-          fontSize: '18px'
-        }}
-      >
-        🗑️
-      </span>
-    </div>
-
-    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '12px' }}>
-    <div>
-        <label>Categoria:</label>
-        <select value={m.categoria} onChange={(e) => handleChange(index, 'categoria', e.target.value)}>
-          <option value="">Selecione</option>
-          {categoriaUnicos.map((e, i) => <option key={i} value={e}>{e}</option>)}
-        </select>
-      </div>
-
-      <div>
-        <label>Data:</label>
-        <input type="date" value={m.data} onChange={(e) => handleChange(index, 'data', e.target.value)} />
-      </div>
-
-      <div>
-        <label>Empresa:</label>
-        <select value={m.empresa} onChange={(e) => handleChange(index, 'empresa', e.target.value)}>
-          <option value="">Selecione</option>
-          {empresas.map((e, i) => <option key={i} value={e}>{e}</option>)}
-        </select>
-      </div>
-
-      <div>
-        <label>Operação:</label>
-        <select value={m.operacao} onChange={(e) => handleChange(index, 'operacao', e.target.value)}>
-          <option value="">Selecione</option>
-          {operacoesFiltradas.map((op, i) => <option key={i} value={op}>{op}</option>)}
-        </select>
-      </div>
-
-      <div>
-        <label>Solicitante:</label>
-        <input type="text" value={m.solicitante} disabled />
-      </div>
-
-      <div className="item">
-            <label>Valor:</label>
-            <input
-              type="number"
-              value={m.valor}
-              onChange={(e) => handleChange(index, 'valor', e.target.value)}
-              placeholder="R$"
-            />
-          </div>
-    </div>
-
-    {/* Bloco separado para Valor e Endereço */}
-    <div style={{ marginTop: '1rem', display: 'flex', flexDirection: 'column', gap: '12px' }}>
-           <div>
-        <label>Obs:</label>
-        <input
-          type="text"
-          value={m.endereço}
-          onChange={(e) => handleChange(index, 'endereço', e.target.value)}
-          style={{ width: '90%', fontSize: '16px' }}
-        />
-      </div>
-    </div>
-  </div>
-))}
-
-
-<div style={{ textAlign: 'center', marginTop: '0.5rem' }}>
-        <span onClick={adicionarLinha} style={{ fontSize: '20px', background: '#e0e0e0', padding: '4px 10px', borderRadius: '4px' }}>
-          ➕
-        </span>
-      </div>
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-  <input
-    type="file"
-    accept="image/*"
-    onChange={(e) => setFile(e.target.files[0])}
-    style={{ marginBottom: '1rem' }}
-  />
-  <br />
-  <button
-    onClick={handleEnviar}
-    style={{
-      background: '#141e7d',
-      color: '#fff',
-      padding: '10px 20px',
-      borderRadius: '6px',
-      fontSize: '14px'
-    }}
-  >
-    📩 Enviar Solicitação + Anexo
-  </button>
-</div>
-
-
-        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-          <button onClick={handleEnviar} style={{ background: '#141e7d', color: '#fff', padding: '10px 20px', borderRadius: '6px', fontSize: '14px' }}>
-          📩 Enviar Solicitação
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', background: '#f8f8f8', fontFamily: 'sans-serif', padding: '2rem' }}>
+      <div style={{ background: '#fff', padding: '2rem', borderRadius: '8px', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}>
+        {/* Cabeçalho com logo e botão voltar */}
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '1rem' }}>
+          <img src="/artte.ico" alt="Logo" style={{ height: '110px' }} />
+          <button onClick={() => router.push('/login')} style={{ background: '#141e7d', color: '#fff', border: 'none', padding: '8px 10px', borderRadius: '6px', cursor: 'pointer', fontSize: '14px', width: '80px', textAlign: 'center' }}>
+            ⬅ Voltar
           </button>
-        </div>             
+        </div>
+
+        <center><h2 style={{ marginBottom: '1rem' }}>Solicitação de Reembolso</h2></center>
+        
+        {marmitas.map((m, index) => (
+          <div key={index} style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '6px', marginBottom: '10px', background: '#f9f9f9', width: '350px' }}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
+              <span onClick={() => removerLinha(index)} style={{ background: 'none', border: 'none', cursor: 'pointer', fontSize: '18px' }}>🗑️</span>
+            </div>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '12px' }}>
+              <div>
+                <label>Categoria:</label>
+                <select value={m.categoria} onChange={(e) => handleChange(index, 'categoria', e.target.value)}>
+                  <option value="">Selecione</option>
+                  {categoriaUnicos.map((e, i) => <option key={i} value={e}>{e}</option>)}
+                </select>
+              </div>
+              <div>
+                <label>Data:</label>
+                <input type="date" value={m.data} onChange={(e) => handleChange(index, 'data', e.target.value)} />
+              </div>
+              <div>
+                <label>Empresa:</label>
+                <select value={m.empresa} onChange={(e) => handleChange(index, 'empresa', e.target.value)}>
+                  <option value="">Selecione</option>
+                  {empresas.map((e, i) => <option key={i} value={e}>{e}</option>)}
+                </select>
+              </div>
+              <div>
+                <label>Operação:</label>
+                <select value={m.operacao} onChange={(e) => handleChange(index, 'operacao', e.target.value)}>
+                  <option value="">Selecione</option>
+                  {operacoesFiltradas.map((op, i) => <option key={i} value={op}>{op}</option>)}
+                </select>
+              </div>
+              <div>
+                <label>Solicitante:</label>
+                <input type="text" value={m.solicitante} disabled />
+              </div>
+              <div>
+                <label>Valor:</label>
+                <input type="number" value={m.valor} onChange={(e) => handleChange(index, 'valor', e.target.value)} placeholder="R$" />
+              </div>
+            </div>
+            <div style={{ marginTop: '1rem', display: 'flex', flexDirection: 'column', gap: '12px' }}>
+              <div>
+                <label>Obs:</label>
+                <input type="text" value={m.endereço} onChange={(e) => handleChange(index, 'endereço', e.target.value)} style={{ width: '90%', fontSize: '16px' }} />
+              </div>
+            </div>
+          </div>
+        ))}
+
+        
+        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+          <input type="file" accept="image/*" onChange={(e) => setFile(e.target.files[0])} style={{ marginBottom: '1rem' }} />
+          <br />
+          <button onClick={compartilharTexto} style={{ background: '#141e7d', color: '#fff', padding: '10px 20px', borderRadius: '6px', fontSize: '14px', marginRight: '1rem' }}>
+            📄 Enviar Solicitação
+          </button>
+          <button onClick={compartilharComprovante} style={{ background: '#2e7d32', color: '#fff', padding: '10px 20px', borderRadius: '6px', fontSize: '14px' }}>
+            📎 Enviar Comprovante
+          </button>
+        </div>
       </div>
-
-
     </div>
   );
 }
